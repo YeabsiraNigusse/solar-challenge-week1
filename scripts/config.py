385eb93a69da082e7a488272f5d1eeba49f4b05a
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2025-05-21 08:17:26 +0300

    feat: done with eda-sierraleone analisis task

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,10 @@
 def get_paths():
     return {
-        'RAW_CSV': '../data/benin-malanville.csv',
-        'CLEAN_CSV': '../data/benin_clean.csv'
+        'benin_CSV': '../data/benin-malanville.csv',
+        'sierraleone_CSV': '../data/sierraleone-bumbuna.csv',
+        'togo_CSV': '../data/togo-dapaong_qc.csv',
+        
+        'CLEAN_benin_CSV': '../data/benin_clean.csv',
+        'CLEAN_sierraleone_CSV': '../data/sierraleone_clean.csv',
+        'CLEAN_togo_CSV': '../data/togo_clean.csv',
     }
